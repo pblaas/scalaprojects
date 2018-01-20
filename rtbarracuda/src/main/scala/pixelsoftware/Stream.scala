276892ad58bdd4a229f0a6aa8a23f85b0e4cac45
pblaas <patrick@kite4fun.nl>
2018-01-21 00:41:28 +0100

    update code to support sqlcontext

--- rtbarracuda/src/main/scala/pixelsoftware/Stream.scala
+++ rtbarracuda/src/main/scala/pixelsoftware/Stream.scala
@@ -33,7 +33,7 @@ object Stream {
     val pattern = barracudaLogPattern()
 
     // The only difference from the push example is that we use createPollingStream instead of createStream.
-    val flumeStream = FlumeUtils.createPollingStream(ssc, "xxx.xx.nl", 9988)
+    val flumeStream = FlumeUtils.createPollingStream(ssc, "localhost", 9988)
 
     // This creates a DStream of SparkFlumeEvent objects. We need to extract the actual messages.
     // This assumes they are just strings, like lines in a log file.
@@ -41,10 +41,70 @@ object Stream {
     // could handle structured data if you want.
     val lines = flumeStream.map(x => new String(x.event.getBody().array()))
     // Extract the request field from each log line
-    val requests = lines.map(x => { val matcher: Matcher = pattern.matcher(x); if (matcher.matches()) matcher.group(17) else "[Not match found]" })
+    //val requests = lines.map(x => { val matcher: Matcher = pattern.matcher(x); if (matcher.matches()) matcher.group(17) else "[Not match found]" })
+    //  s"$month $day $time $device  $chain\\: $fqdn$clientip $messageid $bytesin $bytesout $action $sender $receiver $filteraction $filterreason $ip"
 
-    //val requests = lines.map(x => x)
+    val requests = lines.map(x => {
+      val matcher: Matcher = pattern.matcher(x)
+      if (matcher.matches()) {
+        val month = matcher.group(1)
+        val day = matcher.group(2).toInt
+        val time = matcher.group(3)
+        val device = matcher.group(4)
+        val chain = matcher.group(5)
+        val fqdn = matcher.group(6)
+        val clientip = matcher.group(8)
+        val messageid = matcher.group(9)
+        val bytesin = matcher.group(10).toInt
+        val bytesout = matcher.group(11).toInt
+        val action = matcher.group(12)
+        val sender = matcher.group(13)
+        val receiver = matcher.group(14)
+        val filteraction = matcher.group(15).toInt
+        val filterreason = matcher.group(16).toInt
+        val ip = matcher.group(17)
+        (month, day, time, device, chain, fqdn, clientip, messageid, bytesin, bytesout, action, sender, receiver, filteraction, filterreason, ip)
+      } else {
+        ("error", 0, "error", "error", "error", "error", "error", "error", 0, 0, "error", "error", "error", 0, 0, "error")
+      }
+    }).window(Seconds(10))
 
+    // Process each RDD from each batch as it comes in
+    requests.foreachRDD((rdd, time) => {
+      // So we'll demonstrate using SparkSQL in order to query each RDD
+      // using SQL queries.
+
+      // Get the singleton instance of SQLContext
+      val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+      import sqlContext.implicits._
+
+      // SparkSQL can automatically create DataFrames from Scala "case classes".
+      // We created the Record case class for this purpose.
+      // So we'll convert each RDD of tuple data into an RDD of "Record"
+      // objects, which in turn we can convert to a DataFrame using toDF()
+      val requestsDataFrame = rdd.map(w => Record(w._1, w._2, w._3, w._4, w._5, w._6, w._7, w._8, w._9, w._10, w._11, w._12, w._13, w._14, w._15, w._16)).toDF()
+
+      // Create a SQL table from this DataFrame
+      requestsDataFrame.createOrReplaceTempView("requests")
+
+      // Count up occurrences of each user agent in this RDD and print the results.
+      // The powerful thing is that you can do any SQL you want here!
+      // But remember it's only querying the data in this RDD, from this batch.
+      //val wordCountsDataFrame =
+      //  sqlContext.sql("select month, count(*) as total from requests group by month")
+      val wordCountsDataFrame =
+        sqlContext.sql("select * from requests")
+
+      println(s"========= $time =========")
+      wordCountsDataFrame.show()
+
+      // If you want to dump data into an external database instead, check out the
+      // org.apache.spark.sql.DataFrameWriter class! It can write dataframes via
+      // jdbc and many other formats! You can use the "append" save mode to keep
+      // adding data from each batch.
+    })
+
+    /*
     // Extract the URL from the request
     //val urls = requests.map(x => { val arr = x.toString().split(" "); if (arr.size == 3) arr(1) else "[error]" })
     //val urls = requests.map(x => { val arr = x.toString().split(" "); arr(0) })
@@ -56,10 +116,31 @@ object Stream {
     // Sort and print the results
     val sortedResults = urlCounts.transform(rdd => rdd.sortBy(x => x._2, false))
     sortedResults.print()
+    */
 
     // Kick it off
     ssc.checkpoint("/tmp")
     ssc.start()
     ssc.awaitTermination()
+  }
+}
+
+//  s"$month $day $time $device  $chain\\: $fqdn$clientip $messageid $bytesin $bytesout $action $sender $receiver $filteraction $filterreason $ip"
+/** Case class for converting RDD to DataFrame */
+case class Record(month: String, day: Int, time: String, device: String, chain: String, fqdn: String, clientip: String, messageid: String, bytesin: Int, bytesout: Int, action: String, sender: String, receiver: String, filteraction: Int, filterreason: Int, ip: String)
+
+/**
+ * Lazily instantiated singleton instance of SQLContext
+ *  (Straight from included examples in Spark)
+ */
+object SQLContextSingleton {
+
+  @transient private var instance: SQLContext = _
+
+  def getInstance(sparkContext: SparkContext): SQLContext = {
+    if (instance == null) {
+      instance = new SQLContext(sparkContext)
+    }
+    instance
   }
 }
