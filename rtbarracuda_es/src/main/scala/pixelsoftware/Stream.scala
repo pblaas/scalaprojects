635d0741907394e9073af3a017bf01959eb48068
pblaas <patrick@kite4fun.nl>
2018-01-30 23:51:26 +0100

    update driver code

--- rtbarracuda_es/src/main/scala/pixelsoftware/Stream.scala
+++ rtbarracuda_es/src/main/scala/pixelsoftware/Stream.scala
@@ -1,8 +1,6 @@
 
 package pixelsoftware
 
-import scala.collection.mutable.Queue
-
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{ Seconds, StreamingContext, Time }
 import org.apache.spark.storage.StorageLevel
@@ -14,35 +12,28 @@ import org.apache.spark.sql.functions._
 
 import org.elasticsearch.spark._
 import org.elasticsearch.spark.streaming
-import org.elasticsearch.spark.streaming.EsSparkStreaming
-import org.elasticsearch.spark.rdd.EsSpark
+import org.apache.spark.streaming.flume._
 
 import java.util.regex.Pattern
 import java.util.regex.Matcher
-import java.sql.Timestamp
 
 import com.github.nscala_time.time.Imports._
 
 import Utilities._
-
-import org.apache.spark.streaming.flume._
 
 /** Example of connecting to Flume log data, in a "pull" configuration. */
 object Stream {
 
   def main(args: Array[String]) {
 
-    // Create the context with a 1 second batch size
-    //val ssc = new StreamingContext("local[*]", "BarracudaStream", Seconds(1))
-
-    //val conf = new SparkConf().setAppName("BarracudaStream").setMaster("local[*]").set("spark.sql.warehouse.dir", "/tmp")
-    val conf = new SparkConf().setAppName("BarracudaStream").setMaster("local[*]").set("spark.sql.warehouse.dir", "/tmp").set("spark.cores.max", "1")
+    val conf = new SparkConf().setAppName("BarracudaStream").set("spark.sql.warehouse.dir", "/tmp").set("spark.cores.max", "4")
+    //conf.set("master", "local")
     conf.set("es.index.auto.create", "true")
-    conf.set("es.nodes", "127.0.0.1:9200")
+    //conf.set("es.nodes", "elasticsearch.elkspark.svc.cluster.local:9200")
     conf.set("es.mapping.date.rich", "true")
     conf.set("es.ingest.pipeline", "geoip")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val ssc = new StreamingContext(conf, Seconds(10))
 
     setupLogging()
 
@@ -50,7 +41,7 @@ object Stream {
     val pattern = barracudaLogPattern()
 
     // The only difference from the push example is that we use createPollingStream instead of createStream.
-    val flumeStream = FlumeUtils.createPollingStream(ssc, "localhost", 9988)
+    val flumeStream = FlumeUtils.createPollingStream(ssc, "syslog.premiumantispam.nl", 9988)
 
     // This creates a DStream of SparkFlumeEvent objects. We need to extract the actual messages.
     // This assumes they are just strings, like lines in a log file.
@@ -92,7 +83,6 @@ object Stream {
 
       // Get the singleton instance of SQLContext
       val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
-      val timestamp = DateTime.now(DateTimeZone.UTC).toString()
       //val inputStream = ssc.queueStream(rddQueue)
       import sqlContext.implicits._
 
@@ -101,16 +91,12 @@ object Stream {
       // So we'll convert each RDD of tuple data into an RDD of "Record"
       // objects, which in turn we can convert to a DataFrame using toDF()
       val requestsDataFrame = rdd.map(w => Record(w._1, w._2, w._3, w._4, w._5, w._6, w._7, w._8, w._9, w._10, w._11, w._12, w._13, w._14, w._15, w._16)).toDF()
+      val requestsDataFramecount = requestsDataFrame.count()
+      println(requestsDataFramecount)
       val cleandf = requestsDataFrame.na.drop()
       // Create a SQL table from this DataFrame
       cleandf.createOrReplaceTempView("requests")
-      //requestsDataFrame.registerTempTable("tempTable")
-      //requestsDataFrame.write.saveAsTable("pandas")
-      //sqlContext.sql("DROP VIEW global_temp.rq")
-      //requestsDataFrame.createGlobalTempView("rq")
 
-      // Count up occurrences of each user agent in this RDD and print the results.
-      // The powerful thing is that you can do any SQL you want here!
       // But remember it's only querying the data in this RDD, from this batch.
       val wordCountsDataFrame =
         sqlContext.sql("select month, day, time, device, clientip, sender, receiver from requests")
@@ -118,28 +104,9 @@ object Stream {
       println(s"========= $time =========")
       val df = wordCountsDataFrame.groupBy("clientip").count().sort($"count".desc)
       val dfwithtime = df.withColumn("created", lit(System.currentTimeMillis()))
-      dfwithtime.write.format("org.elasticsearch.spark.sql").mode("append").save("spark/docs")
-      //val sortedDF = wordCountsDataFrame.groupBy("clientip")
+      val sparkIndex = s"spark-${{ java.time.LocalDate.now }}/docs"
+      dfwithtime.write.format("org.elasticsearch.spark.sql").mode("append").save(sparkIndex)
       dfwithtime.show(15)
-
-      //wordCountsDataFrame.printSchema()
-      //wordCountsDataFrame.groupBy("sender").count().sort($"count".desc).show(5, false)
-      //val df = wordCountsDataFrame.groupBy("receiver").count().sort($"count".desc)
-
-      //println(df)
-      //df.show(5, false)
-
-      //val batchIps = Ips(timestamp.toString(), "192.168.10.200", 1)
-      //EsSparkStreaming.saveToEs(inputStream, "spark/docs")
-
-      // If you want to dump data into an external database instead, check out the
-      // org.apache.spark.sql.DataFrameWriter class! It can write dataframes via
-      // jdbc and many other formats! You can use the "append" save mode to keep
-      // adding data from each batch.
-
-      //rddQueue.synchronized {
-      //  rddQueue += ssc.sparkContext.makeRDD(Seq(batchIps))
-      //}
     })
 
     // Kick it off
@@ -151,7 +118,6 @@ object Stream {
 
 /** Case class for converting RDD to DataFrame */
 case class Record(month: String, day: Int, time: String, device: String, chain: String, fqdn: String, clientip: String, messageid: String, bytesin: Int, bytesout: Int, action: String, sender: String, receiver: String, filteraction: Int, filterreason: Int, ip: String)
-//case class Ips(date: String, ipaddress: String, count: Int)
 
 /**
  * Lazily instantiated singleton instance of SQLContext
